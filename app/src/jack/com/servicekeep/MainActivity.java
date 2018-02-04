ae818989a0be8ec3ad67d3fef0a3b98a3fe0a93e
xuningjack <xuningjack@126.com>
2018-02-04 16:34:03 +0800

    Jack:增加Toast提示，提升TargetVersion

--- app/src/jack/com/servicekeep/MainActivity.java
+++ app/src/jack/com/servicekeep/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import jack.com.servicekeep.manager.KeepAliveManager;
 
@@ -30,9 +31,11 @@ public class MainActivity extends Activity implements View.OnClickListener{
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.kill_service:
+                Toast.makeText(getApplicationContext(), "kill service", Toast.LENGTH_LONG).show();
                 KeepAliveManager.INSTANCE.stopKeepAliveSerice(MainActivity.this);
                 break;
             case R.id.start_service:
+                Toast.makeText(getApplicationContext(), "start service", Toast.LENGTH_LONG).show();
                 KeepAliveManager.INSTANCE.startKeepAliveService(MainActivity.this);
                 break;
         }
